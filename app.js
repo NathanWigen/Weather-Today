a99c7586bd8aeee3940c68f874cdabc2dc849331
Nathan Wigen <nathanwigen@Nathans-MacBook-Air.local>
2020-09-22 13:43:20 -0700

    new code update

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
-
 const baseUrl = `https://api.oceandrivers.com:443/v1.0/getWeatherDisplay/`
 const stationName = ''
 const afterUrl = `/?period=latestdata`
 
 
-const input = document.querySelector("#blank")
-const button = document.querySelector("#submit")
 
-button.addEventListener('click', async () => {
+const input = document.querySelector("input")
+const form = document.querySelector("form")
+
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
   let stationName = input.value
-  const response = await axios(`${baseUrl}${stationName}${afterUrl}`)
-  console.log(response.data);
+  console.log(stationName)
+  getCity(stationName)
+  // const response = await axios(`${baseUrl}${stationName}${afterUrl}`)
+  // console.log("Data 13", response.data);
   // renderList(response.data.TEMPERATURE)
 })
 
-// const weatherDisplay = document.querySelector(".weather-data")
 
-
-// const renderList = weather => {
-//   weather(data => {
-//     const dataContainer = document.createElement('div')
-//     dataContainer.className = "weather-container"
-
-//     let temp = document.createElement('h4')
-//     temp.innerHTML = `${data.TEMPERATURE}`
-//     weatherContainer.appendChild(temp)
-
-//     let display = document.createElement('p')
-//     display.innerHTML = `${data.WEATHER_DES}`
-//     weatherContainer.appendChild(display)
-
-//   })
-//   weatherDisplay.appendChild(weatherContainer)
-// }
-
-async function getWeather(stationName, period) {
-  const url = `https://api.oceandrivers.com:443/v1.0/getSocibWeatherStation/v1.0/getSocibWeatherStation/${stationName}/${period}/`
+async function getCity(city) {
   try {
-    const response = await axios(url)
-    // console.log(response.data.TEMPERATURE);
-    const temp = response.data.TEMPERATURE
-    weatherTemp(temp)
+    const response = await axios(`${baseUrl}${city}${afterUrl}`)
+    console.log(response.data)
+    let weatherResponse = response.data
+    // console.log(weatherResponse.TEMPERATURE)
+    renderData(weatherResponse)
   } catch (error) {
-    console.log(`Error: ${error}`);
+    console.log(`Error: ${error}`)
   }
 }
 
-function weatherTemp(temp) {
-  const text = document.createElement('h4')
+function renderData(weatherResponse) {
+  let tempEle = document.createElement('p')
+  tempEle.textContent = weatherConversion(weatherResponse.TEMPERATURE)
+  document.querySelector('h2').append(tempEle)
   
+  let condEle = document.createElement('p')
+  condEle.textContent = weatherResponse(weatherResponse.WEATHER_DES)
+  document.querySelector('h2').append(condEle)
 }
+
+// function renderData(weatherResponse) {
+//   let condEle = document.createElement('p')
+//   condEle.textContent = (response.data.WEATHER_DES)
+//   document.querySelector('h2').append(condEle)
+// }
+
+
+ 
+function weatherConversion(data) {
+  return Math.round((data * 1.8) + 32)
+}
+// let seattle = weatherConversion(25.3)
+// console.log('Seattle:', Math.round(seattle))
